8840780f4c9ec9c730ca4a9054d8b4a571b6a74f
GoForBroke <go.for.broke1006@gmail.com>
2026-03-11 15:22:00 +0200

    Initial commit

---
+++ scrape_core/scanner/final_enrichment.py
@@ -0,0 +1,70 @@
+from typing import List, Any
+
+from scrape_core.common import ScrapeResult
+from scrape_core.scanner.pipeline import StandardPipeline
+
+
+def result_final_enrichment(
+        results: List[ScrapeResult],
+        pipeline: StandardPipeline,
+        driver: Any,
+        datum: Any,
+        group_el: Any,
+        robots_txt_allows: bool = False,
+) -> List[ScrapeResult]:
+    """
+    After method StandardPipeline::scan_card build object,
+    we try to fill the gaps with some additional data if its were missed.
+    
+    @param results:  List[ScrapeResult] - what we found on the web page
+    @param pipeline: StandardPipeline   - the scanning process metadata and instructions
+    @param driver:   WebDriver          - accessor to page content
+    @param datum:    Any                - accessor to scanning context
+    """
+    
+    import copy
+    from scrape_core.contract.prime.geo.address import AddressInfo
+    from scrape_core.contract.prime.price.price_info import PriceInfo
+    
+    categories_list = pipeline.load_categories(driver, datum)
+    categories_list: List[str] | None = copy.deepcopy(categories_list) if categories_list is not None else []
+    page_title = pipeline.load_page_title(driver, datum)
+    if page_title is not None:
+        print('INFO: load page title', f'"{page_title}"')
+        categories_list.append(page_title)
+    if group_el is not None:
+        group_title = pipeline.load_group_title(driver, datum, group_el)
+        if group_title is not None:
+            categories_list.append(group_title)
+    if categories_list:
+        print('INFO: load page categories', categories_list)
+    
+    for res in results:
+        res.provider = pipeline.get_provider()
+        
+        if res.country is None:
+            res.country = pipeline.default_country(driver, datum)
+        
+        # custom enrichment for RealEstateListing
+        if hasattr(res, 'address') and isinstance(getattr(res, 'address'), AddressInfo):
+            if getattr(res, 'address').country is None:
+                getattr(res, 'address').country = pipeline.default_country(driver, datum)
+            
+            if getattr(res, 'address').city is None:
+                res.address.city = pipeline.default_city(driver, datum)
+        
+        if res.categories is None:
+            res.categories = categories_list
+        
+        # universal enrichment for all price-info fields
+        for attr in dir(res):
+            if attr.startswith('__') or callable(getattr(res, attr)):
+                continue
+            
+            if isinstance(getattr(res, attr), PriceInfo):
+                if getattr(res, attr).currency is None and not getattr(res, attr).is_empty():
+                    getattr(res, attr).currency = pipeline.default_currency(driver, datum)
+        
+        res.robots_txt_allows = robots_txt_allows
+    
+    return results
---
+++ scrape_core/scanner/pipeline.py
@@ -0,0 +1,93 @@
+from abc import ABC, abstractmethod
+from typing import List, Tuple, Any, Generator
+
+from scrape_core.common import ScrapeResult
+from scrape_core.contract.prime.country_code.country_code import CountryAlpha2Code
+from scrape_core.contract.prime.currency.currency_code import Currency3Code
+
+
+class StandardPipeline(ABC):
+    
+    @abstractmethod
+    def get_domain(self) -> str:
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def get_provider(self) -> str:
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def threshold_empty_pages(self) -> int:
+        return 5
+    
+    @abstractmethod
+    def default_country(self, driver, datum) -> CountryAlpha2Code | None:
+        return None
+    
+    @abstractmethod
+    def default_city(self, driver, datum) -> str | None:
+        return None
+    
+    @abstractmethod
+    def default_currency(self, driver, datum) -> Currency3Code | None:
+        return None
+    
+    @abstractmethod
+    def http_user_agent(self) -> str | None:
+        return None
+    
+    @abstractmethod
+    def main_page_link(self) -> str | None:
+        return None
+    
+    @abstractmethod
+    def get_robots_txt_content(self) -> str:
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def before_scanning(self, driver) -> None:
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def get_data(self) -> List[str | Tuple[Any] | List[Any]]:
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def before_datum(self, driver, datum: Any) -> None:
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def apply_datum(self, driver, datum: Any) -> None:
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def after_datum(self, driver, datum: Any) -> None:
+        return None
+    
+    @abstractmethod
+    def load_categories(self, driver, datum: Any) -> List[str] | None:
+        return None
+    
+    @abstractmethod
+    def load_page_title(self, driver, datum: Any) -> str | None:
+        return None
+    
+    @abstractmethod
+    def load_groups(self, driver, datum: Any) -> List[Any] | None:
+        return None
+    
+    @abstractmethod
+    def load_group_title(self, driver, datum: Any, group) -> str | None:
+        return None
+    
+    @abstractmethod
+    def load_cards(self, driver, datum: Any, group) -> List[Any]:
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def scan_card(self, driver, datum, group, card) -> ScrapeResult | None:
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def go_to_next_page(self, driver, datum: Any) -> Generator[bool, Any, None]:
+        yield False
