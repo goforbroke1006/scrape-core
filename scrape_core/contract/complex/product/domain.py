da34c63eb282104a16c046284ab388e8ae200adb
GoForBroke <go.for.broke1006@gmail.com>
2026-03-16 19:50:32 +0200

    Initial commit

---
+++ scrape_core/contract/complex/product/domain.py
@@ -0,0 +1,49 @@
+from dataclasses import dataclass, field
+from fractions import Fraction
+from typing import Optional
+
+from scrape_core.common import ScrapeResult
+from scrape_core.contract.prime.count_info import CountInfo
+from scrape_core.contract.prime.country_code.country_code import CountryAlpha2Code
+
+
+@dataclass
+class ProductBrandInfo:
+    brand_name: Optional[str] = None
+    manufacturer_name: Optional[str] = None
+    manufacturer_country: Optional[CountryAlpha2Code] = None
+    importer_name: Optional[str] = None
+    importer_country: Optional[CountryAlpha2Code] = None
+
+
+@dataclass
+class ProductBarcodeInfo:
+    ean13: Optional[str] = None
+    upc: Optional[str] = None
+    sku: Optional[str] = None
+    internal_id: Optional[str] = None
+
+
+@dataclass
+class ComputingCharacteristics:
+    motherboard_model: Optional[str] = None
+    cpu_model: Optional[str] = None
+    cpu_count: Optional[int] = None
+    ram_model: Optional[str] = None
+    ram_volume_gb: Optional[int | Fraction] = None
+    gpu_model: Optional[str] = None
+    gpu_volume_gb: Optional[int | Fraction] = None
+
+
+@dataclass
+class ProductAvailability:
+    in_stock: Optional[bool] = None
+    stock_quantity: CountInfo = field(default_factory=CountInfo)
+
+
+@dataclass
+class ProductListing(ScrapeResult):
+    brand_info: ProductBrandInfo = field(default_factory=ProductBrandInfo)
+    barcode_info: ProductBarcodeInfo = field(default_factory=ProductBarcodeInfo)
+    computing_characteristics: ComputingCharacteristics = field(default_factory=ComputingCharacteristics)
+    availability: ProductAvailability = field(default_factory=ProductAvailability)
